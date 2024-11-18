73e7b555041c2364bc61f3b2e5ba925d255d14fe
Jaivir Sohal <js1424@ic.ac.uk>
2024-11-18 20:43:31 +0000

    extension completed

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -2,8 +2,22 @@ package journeyplan
 
 // representation of london underground map
 class SubwayMap(val segments: List<Segment>) {
-  fun routesFrom(origin: Station, destination: Station): List<Route> {
-    return findRoutes(origin, destination, listOf()).sortedBy { it.duration() }
+  fun routesFrom(
+    origin: Station,
+    destination: Station,
+    optimisingFor: (Route) -> Int = { it.duration() }
+  ): List<Route> {
+    return filterSuspensions(
+      findRoutes(origin, destination, listOf()).sortedBy(optimisingFor)
+    )
+  }
+
+  // helper function to filter out suspended interchanges
+  fun filterSuspensions(rs: List<Route>): List<Route> {
+    val validRoutes = rs.filter { route ->
+      route.segments.none { it.start.closed }
+    }
+    return validRoutes
   }
 
   fun findRoutes(
@@ -18,7 +32,7 @@ class SubwayMap(val segments: List<Segment>) {
 
     // prevent revisits
     val unvisitedSegments = segments.filter {
-      it.start == current && it.end !in visited
+      it.start == current && it.end !in visited && !it.line.suspended
     }
 
     // if no more valid segments then no routes
@@ -50,27 +64,21 @@ fun londonUnderground(segments: List<Segment>): SubwayMap = SubwayMap(
   segments
 )
 
-fun displayAllRoutes(routes: List<Route>): String {
-  // handle no route
-  if (routes.isEmpty()) return "No travel required."
-
-  // build string
-  var output: String = ""
-
-  for (route in routes) {
-    output += (route.toString() + "\n")
-  }
-
-  return output
-}
-
 // class that defines a specific route
 class Route(val segments: List<Segment>) {
   // calculate change number
-  fun numChanges(): Int = segments.map { it.line }.distinct().size - 1
+  fun numChanges(): Int {
+    return segments
+      .map { it.line }
+      .filter { !(it.suspended) }
+      .distinct()
+      .size - 1
+  }
 
   // calculate duration
-  fun duration(): Int = segments.sumOf { it.avgT }
+  fun duration(): Int = segments
+    .filter { !(it.line.suspended) }
+    .sumOf { it.avgT }
 
   fun omitPath(): String {
     // omitted distinct callings
@@ -101,59 +109,17 @@ class Route(val segments: List<Segment>) {
   }
 }
 
-// create a test map
-val central = Line("Central")
-val newL = Line("New")
-val northActon = Station("North Acton")
-val eastActon = Station("East Acton")
-val whiteCity = Station("White City")
-val shepherdsBush = Station("Shepherd's Bush")
-val hollandPark = Station("Holland Park")
-val nottingHillGate = Station("Notting Hill Gate")
+// function for testing purposes
+fun displayAllRoutes(routes: List<Route>): String {
+  // handle no route
+  if (routes.isEmpty()) return "No travel required."
 
-val map = londonUnderground(
-  listOf(
-    Segment(northActon, nottingHillGate, newL, 11),
-    Segment(northActon, eastActon, central, 2),
-    Segment(eastActon, northActon, central, 2),
-    Segment(eastActon, whiteCity, central, 3),
-    Segment(whiteCity, eastActon, central, 3),
-    Segment(whiteCity, shepherdsBush, central, 1),
-    Segment(shepherdsBush, whiteCity, central, 1),
-    Segment(shepherdsBush, hollandPark, central, 2),
-    Segment(hollandPark, shepherdsBush, central, 2),
-    Segment(hollandPark, nottingHillGate, central, 2),
-    Segment(nottingHillGate, hollandPark, central, 2)
-  )
-)
+  // build string
+  var output: String = ""
 
-val northernLine = Line("Northern")
-val victoriaLine = Line("Victoria")
-val centralLine = Line("Central")
+  for (route in routes) {
+    output += (route.toString() + "\n")
+  }
 
-val highgate = Station("Highgate")
-val archway = Station("Archway")
-val tufnellPark = Station("Tufnell Park")
-val kentishTown = Station("Kentish Town")
-val camden = Station("Camden Town")
-val euston = Station("Euston")
-val warrenStreet = Station("Warren Street")
-val oxfordCircus = Station("Oxford Circus")
-val bondStreet = Station("Bond Street")
-
-val highgateToOxfordCircus =
-  Route(
-    listOf(
-      Segment(highgate, archway, northernLine, 3),
-      Segment(archway, kentishTown, northernLine, 3),
-      Segment(kentishTown, camden, northernLine, 3),
-      Segment(camden, euston, northernLine, 3),
-      Segment(euston, warrenStreet, victoriaLine, 3),
-      Segment(warrenStreet, oxfordCircus, victoriaLine, 3)
-    )
-  )
-
-fun main() {
-  val ss = map.routesFrom(northActon, nottingHillGate)
-  println(displayAllRoutes(ss))
+  return output
 }
--- src/main/kotlin/journeyplan/TravelModel.kt
+++ src/main/kotlin/journeyplan/TravelModel.kt
@@ -1,18 +1,39 @@
 package journeyplan
 
-class Station(val stationName: String) {
+class Station(val stationName: String, var closed: Boolean = false) {
+  fun close() {
+    this.closed = true
+  }
+
+  fun open() {
+    this.closed = false
+  }
+
   override fun toString(): String {
     return stationName
   }
 }
 
-class Line(val lineName: String) {
+class Line(val lineName: String, var suspended: Boolean = false) {
+  fun suspend() {
+    this.suspended = true
+  }
+
+  fun resume() {
+    this.suspended = false
+  }
+
   override fun toString(): String {
     return "$lineName Line"
   }
 }
 
-class Segment(val start: Station, val end: Station, val line: Line, val avgT: Int) {
+class Segment(
+  val start: Station,
+  val end: Station,
+  val line: Line,
+  val avgT: Int
+) {
   override fun toString(): String {
     return "${start.stationName} " +
       "-> ${end.stationName} " +
